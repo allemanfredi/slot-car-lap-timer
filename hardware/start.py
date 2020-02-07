364bc06780d50e2fcc9d2143edf74a8b83e5d84d
allemanfredi <alle.manfredi@gmail.com>
2020-02-07 14:22:10 +0100

    chore(hardware): adds missing lanes

--- hardware/start.py
+++ hardware/start.py
@@ -9,9 +9,13 @@ SLEEP_TIME = 0.01
 
 IO.setup(14,IO.IN)
 IO.setup(18, IO.IN)
+IO.setup(23,IO.IN)
+IO.setup(24, IO.IN)
 
 l1s = 0
 l2s = 0
+l3s = 0
+l4s = 0
 
 while 1:
     l1 = IO.input(14)
@@ -33,5 +37,25 @@ while 1:
         sys.stdout.write("L2:0")
         sys.stdout.flush()
         l2s = l2s + 1
+
+    l3 = IO.input(23)
+    if l3 == False:
+        sys.stdout.write("L3:1")
+        sys.stdout.flush()
+        l3s = 0
+    elif l3s < MAX_SIGNAL_FOR_LAP:
+        sys.stdout.write("L3:0")
+        sys.stdout.flush()
+        l3s = l3s + 1
+    
+    l4 = IO.input(24)
+    if l4 == False:
+        sys.stdout.write("L4:1")
+        sys.stdout.flush()
+        l4s = 0
+    elif l4s < MAX_SIGNAL_FOR_LAP:
+        sys.stdout.write("L4:0")
+        sys.stdout.flush()
+        l4s = l4s + 1
 
     time.sleep(SLEEP_TIME)
